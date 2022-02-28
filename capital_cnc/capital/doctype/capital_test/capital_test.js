533b27e240790f2f2eb2a81b48d6e3490884b514
bahaaabed <bahaa.9999.sh@gmail.com>
2022-02-28 18:17:51 +0200

    test

---
+++ capital_cnc/capital/doctype/capital_test/capital_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, capital and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('capital TEST', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ capital_cnc/capital/doctype/capital_test/test_capital_test.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, capital and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestcapitalTEST(unittest.TestCase):
+	pass
